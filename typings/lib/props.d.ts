bf5426717925ce3c039553f972aecd9662e2f836
wwalpha <wwalpha@gmail.com>
2018-06-10 23:37:01 +0900

    add property

--- typings/lib/props.d.ts
+++ typings/lib/props.d.ts
@@ -1,9 +1,21 @@
 import * as React from "react";
+import { FormErrors, InjectedFormProps, FormProps } from "redux-form";
+import { Dispatch } from "redux";
 
-interface AppProps extends React.Props<{}> { }
-interface SignInProps extends React.Props<{}> { }
-interface SignUpProps extends React.Props<{}> { }
-interface ConfirmProps extends React.Props<{}> { }
-interface ConfirmProps extends React.Props<{}> { }
-interface ForgotProps extends React.Props<{}> { }
-interface ForgotRenewProps extends React.Props<{}> { }
+export interface AppProps extends React.Props<{}> { }
+
+export interface SignInProps extends React.Props<{}> {
+  username: string,
+  password: string,
+}
+
+export interface SignInFormProps extends InjectedFormProps<SignInProps, {}> {
+  dispatch?: Dispatch<{}>;
+  onSubmit: (values: SignInProps, dispatch: Dispatch<{}>, props: SignInFormProps) => void | FormErrors<FormData> | Promise<any>;
+}
+
+export interface SignUpProps extends React.Props<{}> { }
+export interface ConfirmProps extends React.Props<{}> { }
+export interface ConfirmProps extends React.Props<{}> { }
+export interface ForgotProps extends React.Props<{}> { }
+export interface ForgotRenewProps extends React.Props<{}> { }
